caa2c23dd1ec4c1acf617426a5654263c4ac161b
venkey <venkeyvenkatesh885@gmail.com>
2020-09-27 19:03:01 +0530

    [venkey] added attendance

--- Assignment_0/Program.cs
+++ Assignment_0/Program.cs
@@ -8,6 +8,20 @@ namespace Assignment_0
         {
             Console.WriteLine("Hello Employee");
 
+
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+         
         }
     }
 }
