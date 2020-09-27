4e2a194c4622ada7bd78673175e85ba8f0c80d7f
venkey <venkeyvenkatesh885@gmail.com>
2020-09-27 19:18:15 +0530

    [venkey] calculted daily wage

--- Assignment_0/Program.cs
+++ Assignment_0/Program.cs
@@ -9,18 +9,26 @@ namespace Assignment_0
             Console.WriteLine("Hello Employee");
 
 
-            int IS_FULL_TIME = 1;
+            int IS_FULLTIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+
+            if(empCheck==IS_FULLTIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
 
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("Emp Wage :" + empWage);
          
         }
     }
